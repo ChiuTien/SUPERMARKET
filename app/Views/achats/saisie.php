edf45021bd0a26ad8f85cae5d9dac79e7a0259e2
VlexandreMG <dodolashmou@gmail.com>
2026-06-17 11:53:19 +0300

    Saisie.php

---
+++ app/Views/achats/saisie.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+    <!-- Produit  -->
+    <label for="produit"> Produit </label>
+    <input type="text" name="produit" id=""produit><br>
+    <!-- Quantite -->
+     <label for="quantite"> Quantite </label>
+    <input type="text" name="quantite" id=""produit><br>
+    <button type="submit"> Valider </button>
+    </form>
+</body>
+</html>
