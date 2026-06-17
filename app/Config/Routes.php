606571895474e2edf38c19721d0bccc9651444af
ChiuTien <chiumandresychristian@gmail.com>
2026-06-17 10:13:08 +0300

    Premiere affichage

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\ProduitController;
 
 /**
  * @var RouteCollection $routes
@@ -8,4 +9,11 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 //Caisse 
+<<<<<<< HEAD
 $routes->get('/caisse/numero', 'CaisseController::getPage');
+=======
+$routes->get('/caisse/numero', 'Caisse::getPage');
+
+//TEST
+$routes->get('/test','ProduitController::index');
+>>>>>>> 68b1eb5 (Premiere affichage)
