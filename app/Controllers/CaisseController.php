821865a1acf8c87e09fbfbc0bcd28aeffc756b78
VlexandreMG <dodolashmou@gmail.com>
2026-06-17 11:07:39 +0300

    traitAchat done

--- app/Controllers/CaisseController.php
+++ app/Controllers/CaisseController.php
@@ -1,9 +1,39 @@
 <?php 
 namespace App\Controllers;
 
+use App\Models\Achat;
+use App\Models\Caisse;
+
+
 class CaisseController extends BaseController {
+    protected $achat;
+    protected $caisse;
+
+    public function __construct() {
+        $this->achat = new Achat();
+        $this->caisse = new Caisse();
+    }
+
     public function getPage() {
         return view("caisse/numCaisse");
+    }
+
+    public function traitAchat() {
+        $idCaisse = $this->request->getPost('idCaisse');
+        $data = [
+            "caisse_id" => $idCaisse,
+            "date" => date('Y-m-d H:i:s')
+        ];
+
+        $this->achat->insert($data);
+        
+        $achatId = $this->achat->getInsertId();
+
+        $session = session();
+
+        $session->set('achatId',$achatId);
+
+        return view("achats/saisie");
     }
 }
 ?>
