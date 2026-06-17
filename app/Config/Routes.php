7da2c43a11737245fc67408fa7bcfc263384c9e8
VlexandreMG <dodolashmou@gmail.com>
2026-06-17 10:35:54 +0300

    Nesorina le test kely

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,11 +9,4 @@ use App\Controllers\ProduitController;
 $routes->get('/', 'Home::index');
 
 //Caisse 
-<<<<<<< HEAD
 $routes->get('/caisse/numero', 'CaisseController::getPage');
-=======
-$routes->get('/caisse/numero', 'Caisse::getPage');
-
-//TEST
-$routes->get('/test','ProduitController::index');
->>>>>>> eb32c14c6a2b93a717a8e25cb5663c3b3489bdeb
